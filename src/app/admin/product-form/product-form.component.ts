2cc51bc430e904c9a61171170c812a70828b2601
unknown <abdosaad2015@gmail.com>
2018-10-05 13:37:39 +0200

    product form 1

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService, cat } from '../../category.service';
 import { Observable } from 'rxjs';
+import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -9,10 +11,14 @@ import { Observable } from 'rxjs';
 })
 export class ProductFormComponent implements OnInit {
   cats$:Observable<cat[]>;
-  constructor(private catServ:CategoryService) {
+  constructor(private catServ:CategoryService,private productServ:ProductService,private route:Router) {
     this.cats$=this.catServ.getCats();
    }
   ngOnInit() {
+  }
+  save(product) {
+    this.productServ.createProduct(product);
+    this.route.navigate(['/admin/products']);
   }
 
 }
